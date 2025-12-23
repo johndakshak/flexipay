594eb75b566c0862967fb2caae433d83aab9f2de
johnD-git005 <johndakshak@gmail.com>
2025-12-23 13:21:45 +0100

    chore: Implemented user toutes and password hash

--- app/routes/users_route.py
+++ app/routes/users_route.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, status, Depends
 from app.models.user_models import User
-from app.schemas.users_schema import UserCreate, UserResponse
+from app.schemas.users_schema import UserCreate, UserResponse, UserUpdate
 from sqlalchemy.orm import Session
 from base import Base
 from database import get_db, Session
@@ -55,17 +55,100 @@ def create_users(user_create: UserCreate, db: Session = Depends(get_db)):
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"Internal server error: {e}"
         )
+
+@router.get('/', response_model=list[UserResponse])
+def get_all_users(db: Session = Depends(get_db)):
+    all_users = db.query(User).all()
+    return all_users
+
+@router.get('/{user_id}', response_model=list[UserResponse])
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user=db.query(User).filter(User.id == user_id).first()
+
+    if not user:
+        raise HTTPException (
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User ID: {user_id} not found!"
+        )
     
-def raiseError(e):
-    logger.error(f"failed to create record: {e}")
-    raise HTTPException(
-        status_code=status.HTTP_400_BAD_REQUEST,
-        detail={
-            "status": "error",
-            "message": "failed to create user: {e}",
-            "datetime": f"{datetime.utcnow()}"
-        }
+    return user
+
+@router.put('/update/{user_id}', response_model=UserResponse)
+def update_user(user_id: int, user_update: UserUpdate, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"User: '{user_id}' not found!"
+        )
+
+    phone_exist = db.query(User).filter(User.phone == user_update.phone).first()
+    if phone_exist:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail = f"Phone: '{user_update.phone}' already exist!"
     )
+
+    email_exist = db.query(User).filter(User.email == user_update.email).first()
+    if email_exist:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"email: '{user_update.email}' already exist"
+        )
+
+    for field, value in user_update.dict(exclude_unset=True).items():
+        setattr(user, field, value)
+
+    db.commit()
+    db.refresh(user)
+
+    return user
+
+@router.patch('/{user_id}', response_model=UserResponse)
+def patch_user(user_id: int, user_update: UserUpdate, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"User: {user_id} not found!"
+        )
+    
+    phone_exist = db.query(User).filter(User.phone == user_update.phone).first()
+    if phone_exist:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Phone: {user_update.phone} already exist"
+        )
+    
+    email_exist = db.query(User).filter(User.email == user_update.email).first()
+    if email_exist:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"email: '{user_update.email}' already exist"
+        )
+    
+    for field, value in user_update.dict(exclude_unset=True).items():
+        setattr(user, field, value)
+
+    db.commit()
+    db.refresh(user)
+    return user
+
+@router.delete('/{user_id}')
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"User: {user_id} not found!"
+        )
+    
+    db.delete(user)
+    db.commit()
+    return {
+        "message": f"User: {user_id} deleted succssfully!"
+        }
 
 def hash_password(password) -> str:
     salts = bcrypt.gensalt(rounds=12)
--- app/schemas/users_schema.py
+++ app/schemas/users_schema.py
@@ -1,15 +1,202 @@
-from pydantic import BaseModel, EmailStr, Field, field_validator
+from pydantic import BaseModel, EmailStr,field_validator, Field, model_validator
 import re
 from enums import Gender
 from datetime import datetime
+from typing import Optional
 
 class UserCreate(BaseModel):
-    name: str
+    name: str 
     phone: str
     email: EmailStr
-    password: str
+    password: str 
     gender: Gender
-    location: str
+    location: str 
+
+    @field_validator('name')
+    def validate_name(cls, v):
+        v = v.strip()  
+        if not v:
+            raise ValueError('Name cannot be empty!')
+        
+        if len(v) < 3:
+            raise ValueError("Name must be atleast 3 characters long")
+        
+        if len(v) > 30:
+            raise ValueError("Name cannot be more than 30 characters long")
+        
+        return v
+    
+    @field_validator('phone')
+    def validate_phone(cls, value):
+        value = value.strip()  
+        if not value:
+            raise ValueError('Phone cannot be empty!')
+    
+        if not value.isdigit():
+            raise ValueError("Phone number must contain only digits")
+
+        if len(value) != 11:
+            raise ValueError("Phone number must be exactly 11 digits")
+
+        return value
+
+    
+    @field_validator("password")
+    def validate_password(cls, v):
+        v = v.strip()  
+        if not v:
+            raise ValueError('Password cannot be empty!')
+    
+        if len(v) < 6:
+            raise ValueError("Password must be at least 6 characters long")
+
+        has_lower = False
+        has_upper = False
+        has_digit = False
+        has_special = False
+
+        specials = "@$!%*#?&"
+
+        for passwd in v:
+            if passwd.islower():
+                has_lower = True
+            elif passwd.isupper():
+                has_upper = True
+            elif passwd.isdigit():
+                has_digit = True
+            elif passwd in specials:
+                has_special = True
+
+        if not has_lower:
+            raise ValueError("Password must contain a lowercase letter")
+
+        if not has_upper:
+            raise ValueError("Password must contain an uppercase letter")
+
+        if not has_digit:
+            raise ValueError("Password must contain a number")
+
+        if not has_special:
+            raise ValueError("Password must contain a special character (@$!%*#?&)")
+
+        return v
+    
+    @field_validator('location')
+    def validate_location(cls, v):
+        v = v.strip()  
+        if not v:
+            raise ValueError('location cannot be empty!')
+        
+        if len(v) < 3:
+            raise ValueError("location must be atleast 3 characters long")
+        
+        if len(v) > 255:
+            raise ValueError("location cannot be more than 30 characters long")
+        
+        return v
+
+    
+    # @model_validator(mode='after')
+    # def validate_confirm_password(self):
+    #     if self.password != self.confirm_password:
+    #         raise ValueError('passwords must match')
+    #     return self
+
+class UserUpdate(BaseModel):
+    name: Optional[str] = None
+    phone: Optional[str] = None
+    email: Optional[EmailStr] = None
+    gender: Optional[Gender] = None
+    location: Optional[str] = None
+
+    @field_validator('name')
+    def validate_name(cls, v):
+        v = v.strip()  
+        if not v:
+            raise ValueError('Name cannot be empty!')
+        
+        if len(v) < 3:
+            raise ValueError("Name must be atleast 3 characters long")
+        
+        if len(v) > 30:
+            raise ValueError("Name cannot be more than 30 characters long")
+        
+        return v
+    
+    @field_validator('phone')
+    def validate_phone(cls, value):
+        value = value.strip()  
+        if not value:
+            raise ValueError('Phone cannot be empty!')
+    
+        if not value.isdigit():
+            raise ValueError("Phone number must contain only digits")
+
+        if len(value) != 11:
+            raise ValueError("Phone number must be exactly 11 digits")
+
+        return value
+
+    
+    @field_validator("password")
+    def validate_password(cls, v):
+        v = v.strip()  
+        if not v:
+            raise ValueError('Password cannot be empty!')
+    
+        if len(v) < 6:
+            raise ValueError("Password must be at least 6 characters long")
+
+        has_lower = False
+        has_upper = False
+        has_digit = False
+        has_special = False
+
+        specials = "@$!%*#?&"
+
+        for passwd in v:
+            if passwd.islower():
+                has_lower = True
+            elif passwd.isupper():
+                has_upper = True
+            elif passwd.isdigit():
+                has_digit = True
+            elif passwd in specials:
+                has_special = True
+
+        if not has_lower:
+            raise ValueError("Password must contain a lowercase letter")
+
+        if not has_upper:
+            raise ValueError("Password must contain an uppercase letter")
+
+        if not has_digit:
+            raise ValueError("Password must contain a number")
+
+        if not has_special:
+            raise ValueError("Password must contain a special character (@$!%*#?&)")
+
+        return v
+    
+    @field_validator('location')
+    def validate_location(cls, v):
+        v = v.strip()  
+        if not v:
+            raise ValueError('location cannot be empty!')
+        
+        if len(v) < 3:
+            raise ValueError("location must be atleast 3 characters long")
+        
+        if len(v) > 255:
+            raise ValueError("location cannot be more than 30 characters long")
+        
+        return v
+
+    # @model_validator(mode='after')
+    # def validate_confirm_password(self):
+    #     if self.password != self.confirm_password:
+    #         raise ValueError('passwords must match')
+    #     return self
 
 class UserResponse(BaseModel):
     id: int
@@ -17,7 +204,6 @@ class UserResponse(BaseModel):
     phone: str
     email: EmailStr
     location: str
-    role: str
     created_at: datetime
     updated_at: datetime
 
